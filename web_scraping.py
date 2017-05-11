522e956b93e5fd2c182e224e37a15c290c2d7923
aotoole <aotoole@fwparker.org>
2017-05-11 11:00:44 -0500

    d

--- web_scraping.py
+++ web_scraping.py
@@ -1,24 +1,32 @@
 from bs4 import BeautifulSoup
 import urllib.request
 
-url = 'http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage'
-
-page = urllib.request.urlopen(url)
-
-soup = BeautifulSoup(page.read(), 'html.parser')
-
-week_list=[]
-
-
-
-
 #Find Date
+
 def date_list():
+    week_list = []
+    page = urllib.request.urlopen('http://www.fandango.com/regalwebsterplace11_aaaxr/theaterpage')
+    soup = BeautifulSoup(page.read(), 'html.parser')
+    soup.prettify()
+    url_list = [x.get('href') for x in soup.findAll('a', {'class' : 'date-area'})][:7]
     for i in range(7):
-         week_list.append([x.text.strip() for x in soup.findAll('div', {'class' : 'date-week-day'})][i] + " " + [x.text.strip() for x in soup.findAll('div', {'class' : 'date-day'})][i] + " " + [x.text.strip() for x in soup.findAll('div', {'class' : 'date-month'})][i])
-    return week_list
+        week_list.append([x.text.strip() for x in soup.findAll('div', {'class': 'date-week-day'})][i] + " " + [x.text.strip() for x in soup.findAll('div', {'class': 'date-day'})][i] + " " + [x.text.strip() for x in soup.findAll('div', {'class': 'date-month'})][i])
+    return week_list, url_list
 
+#Find movie titles
 
+def movie_title(url):
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read(), 'html.parser')
+    soup.prettify()
+    movie_list = [x.text.strip() for x in soup.findAll('a', {'class': 'dark showtimes-movie-title'})]
+    return movie_list
 
+#Find times
 
-
+def find_time(url):
+    page = urllib.request.urlopen(url)
+    soup = BeautifulSoup(page.read(), 'html.parser')
+    soup.prettify()
+    time_list = [[z.text.strip() for z in y.findAll('time', {'class': 'timeInfo'})] for y in [x for x in soup.findAll('div', {'class': 'showtimes-movie-container'})]]
+    return time_list
