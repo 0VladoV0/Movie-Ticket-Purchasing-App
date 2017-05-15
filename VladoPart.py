713e0a69edaec1e4a577a401ac5f0530d8265024
VladoVladVlaVlV <vvojdanovski@fwparker.org>
2017-05-15 13:15:28 -0500

    k

--- VladoPart.py
+++ VladoPart.py
@@ -6,7 +6,7 @@ from web_scraping import date_list
 from tkinter import *
 from tkinter import font
 import random
-from web_scraping import date_list
+from web_scraping import *
 
 
 class App():
@@ -17,23 +17,38 @@ class App():
         self.myString2.set("Choose Your Movie")
         self.myString3 = StringVar()
         self.myString3.set("Choose Your Month")
+        self.master = master
+
+        self.list_days, self.list_links = date_list()
+
+        print(self.list_days)
 
         self.title_font = font.Font(family="Comic Sans MS", size=17)
         self.new_font = font.Font(family="Comic Sans MS", size=23)
 
-        self.choose_day = OptionMenu(master, self.myString, *date_list()).grid(row="2", column="2")
+        self.choose_day = OptionMenu(master, self.myString, *self.list_days)
+        self.choose_day.grid(row="2", column="2")
 
         self.submit_button = Label(text="Choose Date", bg="lightblue", fg="black")
         self.submit_button.bind('<Button-1>', self.continuemov)
         self.submit_button.grid(row="2", column="3")
 
-        self.choose_movie = OptionMenu(master, self.myString2, date_list())
+        self.submit_button2 = Label(text="Choose Movie", bg="lightblue", fg="black")
 
-    def continuemov(self, master):
+    def continuemov(self, event=None):
+        x = self.list_days.index(self.myString.get())
+        y = self.list_links[x]
+        movie_listings = movie_title(y)
+        self.choose_movie = OptionMenu(self.master, self.myString2, *movie_listings)
         self.choose_movie.grid(row="3", column="2")
+        self.submit_button.config(state="disabled")
 
+        self.submit_button2.bind('<Button-1>', self.continuehour)
+        self.submit_button2.grid(row="3", column="3")
         # if self.myString!="Choose Your Date":
         # self.choose_movie=OptionMenu(master,self.myString2)
+
+    def continuehour(self, event=None):
 
 
 if __name__ == "__main__":
@@ -42,4 +57,3 @@ if __name__ == "__main__":
     my_app = App(root)
 
     root.mainloop()
-
